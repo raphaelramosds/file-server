fc77282d2a6d12e9d8962c1d288e564a73282ec6
Raphael Ramos <raphael.opensource@gmail.com>
2025-05-17 20:01:48 -0300

    feat: upload html page

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-import http from 'http';
-import fs from 'fs/promises';
+import fs from 'fs';
+import fsPromise from 'fs/promises';
 import formidable from 'formidable';
+import http from 'http';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer(async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     if (req.method == 'GET' && req.url == '/') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Server is alive!' }));
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream('src/upload.html').pipe(res);
     }
 
     else if (req.method === 'POST' && req.url === '/file') {
+        
         const form = formidable({});
         let filepath = null;
         let fields;
         let files;
+
+        res.setHeader('Content-Type', 'application/json');
 
         try {
             [fields, files] = await form.parse(req);
@@ -31,8 +34,8 @@ const server = http.createServer(async (req, res) => {
 
             if (!filepath) throw new Error('There is no file uploaded');
 
-            const data = await fs.readFile(filepath, { encoding: 'utf8' });
-            console.log('File content:');
+            const data = await fsPromise.readFile(filepath, { encoding: 'utf8' });
+            console.log(`Content of ${filepath} is:`);
             console.log(data);
 
         } catch (err) {
@@ -42,8 +45,8 @@ const server = http.createServer(async (req, res) => {
             return;
         }
 
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'File upload successfully' }, null, 2));
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream('src/upload.html').pipe(res);
     }
 
     else {
